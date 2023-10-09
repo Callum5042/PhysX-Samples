23505d7e9984982523db2602bce1722cd9cd6905
Callum <Callum5042@gmail.com>
2023-10-09 21:01:41 +0100

    Dynamic SDF project (#1)
    * Added project
    
    * SDF collisions

---
+++ DynamicSDF/Application.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <SDL_video.h>
+#include "Timer.h"
+#include "DxRenderer.h"
+#include "DxShader.h"
+#include "DxLineShader.h"
+#include "DxCamera.h"
+
+#include "DynamicModel.h"
+#include "PlaneModel.h"
+
+#include "Physics.h"
+
+class Applicataion
+{
+public:
+	Applicataion() = default;
+	virtual ~Applicataion();
+
+	int Execute();
+
+private:
+	void DirectXSetup();
+
+	// SDL window
+	bool SDLInit();
+	void SDLCleanup();
+	SDL_Window* m_SdlWindow = nullptr;
+
+	// Timer
+	Timer m_Timer;
+	void CalculateFramesPerSecond();
+
+	// Direct3D 11 renderer
+	std::unique_ptr<DX::Renderer> m_DxRenderer = nullptr;
+	
+	// Direct3D 11 model
+	std::unique_ptr<DX::DynamicModel> m_DynamicModel1 = nullptr;
+	std::unique_ptr<DX::DynamicModel> m_DynamicModel2 = nullptr;
+	std::unique_ptr<DX::DynamicModel> m_DynamicModel3 = nullptr;
+	std::unique_ptr<DX::PlaneModel> m_PlaneModel = nullptr;
+
+	// Direct3D 11 shader
+	std::unique_ptr<DX::Shader> m_DxShader = nullptr;
+	std::unique_ptr<DX::LineShader> m_DxLineShader = nullptr;
+
+	// Direct3D 11 perspective camera
+	std::unique_ptr<DX::Camera> m_DxCamera = nullptr;
+
+	// Update buffers
+	void SetCameraBuffer();
+
+	// Setup directional light
+	void SetupDirectionalLight();
+
+	std::unique_ptr<PX::Physics> m_Physics = nullptr;
+};
